068f50796137e18f1336e7dba66056050aeaf41c
heavy-sea <76517970+heavy-sea@users.noreply.github.com>
2021-05-27 23:03:11 +0800

    Create fire.c

---
+++ fire.c
@@ -0,0 +1,51 @@
+
+#include "controlDevice.h"
+#include <stdlib.h>
+
+
+int fireInit(int pin)
+{
+	pinMode(pin, INPUT);
+	digitalWrite(pin, HIGH);
+
+	//pinMode(fpin,OUTPUT);
+	//digitalWrite(fpin,HIGH);
+}
+
+
+int fireStatusRead(int pin)
+{
+	return digitalRead(pin);
+
+}
+
+
+struct Device fireOn =
+{													//定义火灾结构体
+
+	.deviceName = "fire", 
+	.pin = 25, 
+	.initDevice = fireInit, 
+	.readStatus = fireStatusRead, 
+	.next = NULL
+};
+
+
+//添加浴室灯结构体到链表
+struct Device * addFireToDeviceLinks(struct Device * phead)
+{
+	if (phead == NULL)
+		{
+		return & fireOn;
+		}
+	else 
+		{
+		fireOn.next 		= phead;
+		phead				= &fireOn;
+
+		return phead;
+
+		}
+
+}
+
